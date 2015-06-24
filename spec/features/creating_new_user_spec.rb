2256ab6779f502aaa36b21b73bf1d3226ad7b658
Sam <samuel.farmer@intercity-uk.com>
2015-06-24 09:54:45 +0100

    added feature spec for unsuccessfully creating a new user

--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -11,14 +11,31 @@ feature 'Creating new user' do
     expect(current_path).to eq users_path
     
     click_link 'Add New User'
+    fill_in 'Username', with: 'this is a username'
+    fill_in 'Firstname', with: 'Bob'
+    fill_in 'Lastname', with: 'Loblaw'
     fill_in 'Email', with: 'new_user@email.com'
     fill_in 'Password', with: '123456'
-    fill_in 'Password Confirmation', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
     click_button 'Create User'
 
-    new_user = assigns(:user)
-    expect(current_path).to eq user_path(user.id)
+    expect(page).to have_content 'this is a username'
   end
 
-  scenario 'Unsuccessfully creating a new user' 
+  scenario 'Unsuccessfully creating a new user' do
+    click_link 'Users'
+    expect(current_path).to eq users_path
+
+    click_link 'Add New User'
+    fill_in 'Username', with: 'this is a username'
+    fill_in 'Firstname', with: nil
+    fill_in 'Lastname', with: 'Loblaw'
+    fill_in 'Email', with: 'new_user@email.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '654321'
+    click_button 'Create User'
+
+    expect(page).to have_content "Firstname can't be blank"
+  end
+
 end
