b6c74b2b7223ae9f7768b833d0db2d8aa9bab87f
Sam <samuel.farmer@intercity-uk.com>
2015-06-29 09:59:49 +0100

    added simple partial for the about section, need to add a basic feature spec to test for it

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,13 @@
 class StaticPagesController < ApplicationController
 
+  def about
+    logger.debug "-----------------------"
+    logger.debug "-----------------------"
+    logger.debug "in the about controller action"
+    logger.debug "for static pages"
+    logger.debug "-----------------------"
+    logger.debug "-----------------------"
+    @test_variable = Time.now
+  end
+
 end
