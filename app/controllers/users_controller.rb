a9ce128847ca60d20b5fd51f0071807c9235d26d
Sam <samuel.farmer@intercity-uk.com>
2015-07-02 09:32:29 +0100

    added simple test for deleting a user as admin using a js request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
   private
     def set_user
       @user = User.find(params[:id])
